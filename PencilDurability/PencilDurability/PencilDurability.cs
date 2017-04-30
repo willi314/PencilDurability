9d3b4c01eed49721b7ef1ef533570885b2d28f91
willi314 <willi314@muohio.edu>
2017-04-29 22:38:23 -0400

    Created PencilDurability project and created first test for writing on a paper

---
+++ PencilDurability/PencilDurability/PencilDurability.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PillarPencilDurability
+{
+    public class PencilDurability
+    {
+        private string textOnPaper;
+        public PencilDurability()
+        {
+            textOnPaper = "";
+        }
+
+        public void write(string textToWrite)
+        {
+            textOnPaper += textToWrite;
+        }
+
+        public string checkPage()
+        {
+            return textOnPaper;
+        }
+    }
+}
---
+++ PencilDurability/PillarPencilDurabilityTest/PencilDurabilityTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PillarPencilDurability;
+
+namespace PillarPencilDurabilityTest
+{
+    [TestClass]
+    public class PencilDurabilityTest
+    {
+        [TestMethod]
+        public void WhenPencilWritesOnPageTextAppearsOnPage()
+        {
+            PencilDurability pencil = new PencilDurability();
+            pencil.write("Test text");
+            Assert.AreEqual("Test text", pencil.checkPage());
+        }
+    }
+}
